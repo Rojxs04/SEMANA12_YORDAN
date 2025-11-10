0d03965e642ad372eef36f1672e08b6399c26e63
unknown <gabo2001242e@gmail.com>
2025-11-09 19:34:16 -0600

    subiendo mi nombre y apellido

---
+++ GABRIEL ROJAS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GABRIEL_ROJAS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Que tal Profe soy Gabriel Rojas");
+        }
+    }
+}
